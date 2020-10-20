79de68616b53188bc8cd26ed9c1d87e315727a77
John Spence <john.spence.v@gmail.com>
2020-10-20 03:17:48 -0700

    fix: 🐛 pack removes tarballs before creating a new tarball

--- src/api/private/pack.js
+++ src/api/private/pack.js
@@ -1,5 +1,10 @@
 import { findPackage, yarn, moveFile, getAppRootPath } from '../../util';
+const thenify = require('thenify');
+const glob = thenify(require('glob'));
 const path = require('path');
+const thenifyAll = require('thenify-all');
+const fs = thenifyAll(require('fs'));
+
 
 /**
  * Packs the specified dependency into a tarball in /.poly/dependencies inside the calling project.
@@ -9,13 +14,17 @@ const path = require('path');
  * @returns {String} - Absolute path of the generated tarball.
  */
 export default async function pack(depName, depProject, cwd) {
+  const appRootPath = await getAppRootPath(cwd);
+  const tarballsToRemove = await glob(path.join(appRootPath, `./.poly/dependencies/${depName}*`));
+  for (const tarballPath of tarballsToRemove) {
+    await fs.unlink(tarballPath);
+  }
   await yarn(`pack`, depProject.local_path);
   const depPack = (await findPackage(depProject.local_path)).pack;
   const depVersion = depPack.version;
   const filename = `${depName}-v${depVersion}.tgz`;
   const tarballPath = path.join(depProject.local_path, filename);
-  const myPath = await getAppRootPath(cwd);
-  const newTarballPath = path.join(myPath, `./.poly/dependencies/${filename}`);
+  const newTarballPath = path.join(appRootPath, `./.poly/dependencies/${filename}`);
   await moveFile(tarballPath, newTarballPath);
   return newTarballPath;
 }
