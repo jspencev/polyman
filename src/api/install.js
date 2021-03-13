fd5e0ee0047818d7223d21d1df866a6788cad476
John Spence <john.spence.v@gmail.com>
2021-03-12 22:59:29 -0800

    feat: 🎸 install works outside of the correct repo

--- src/api/install.js
+++ src/api/install.js
@@ -1,29 +1,14 @@
-import { yarn, findRepository, readJSONFile, isSameRepo } from '%/util'
-import { findPackage } from '@jspencev/node-util';
-import path from 'path';
-import { relink, build } from '%/api';
+import { yarn } from '%/util'
+import { relink } from '%/api';
 
 export default async function install(config, cwd) {
-  const {pack, packPath} = await findPackage(cwd);
-  const projectDir = path.parse(packPath).dir;
-  const {sameRepo, repoName} = await isSameRepo(projectDir);
-
   const yarnCmd = ['install'];
   if (config.production) {
     yarnCmd.push('--production');
   }
   await yarn(yarnCmd, cwd);
 
-  // if (rightRepo) {
-  //   await relink({force: true, production: config.production}, cwd);
-  // } else {
-  //   let localDepsToAdd = pack.local_dependencies;
-  //   if (!config.production) {
-  //     localDepsToAdd = localDepsToAdd.concat(pack.local_dev_dependencies);
-  //   }
-  // }
-
-  // if (!config.production) {
-  //   await build({force: true}, cwd);
-  // }
+  config.install = true;
+  config.force = true;
+  await relink(config, cwd);
 }
